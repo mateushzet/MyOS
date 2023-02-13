9cce8d0f73808d9fbb3a24f87005ba73b2d7d75b
mateushzet <mateushzett@gmail.com>
2023-02-13 13:34:30 +0100

    opening animation has been added

--- src/implementation/kernel/main.c
+++ src/implementation/kernel/main.c
@@ -403,5 +403,90 @@ void print_menu() {
 }
 }
 void kernel_main() {
+    //Giving some delay
+    void delay( unsigned int value)
+    {
+        unsigned int count1 =0;
+        unsigned int count2 = 0;
+
+        for(count1 = 0; count1 < value ; count1++ )
+        {
+            for(count2 = 0; count2 < count1 ; count2++ )
+            {
+
+            }
+        }
+    }
+
+
+// string to display Rocket
+    const char rocket[] =
+            "\
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\
+@@@@@@@@@@@@@@@@@@@@@@@@@@,                           ,@@@@@@@@@@@@@@@@@@@@@@@@@\n\
+@@@@@@@@@@@@@@@@@@@/                                         *@@@@@@@@@@@@@@@@@@\n\
+@@@@@@@@@@@@@@                                                     @@@@@@@@@@@@@\n\
+@@@@@@@@@@@                                                           @@@@@@@@@@\n\
+@@@@@@@@                                                                 @@@@@@@\n\
+@@@@@@                                                                     @@@@@\n\
+@@@@       @@@/      *@@@  @@@     @@@         @@@@@@@@      @@@@@@@@       *@@@\n\
+@@@        @@@@      @@@@   @@@   @@@        /@@@    @@@.  ,@@(    @@@        @@\n\
+@@%        @@@@@    @@@@@    @@& @@@         @@@     ,@@(  .@@%                @\n\
+@@         @@@.@@  @@ @@@     @@@@&          @@@     ,@@(    @@@@@@@           @\n\
+@@         @@@ #@@@@/ @@@      @@@           @@@     ,@@(          @@@         @\n\
+@@%        @@@  @@@&  @@@      @@@           @@@.    /@@/    @     *@@,        @\n\
+@@@,       @@@        @@@      @@@            @@@@@@@@@&    @@@@@@@@@@        @@\n\
+@@@@                                                                        *@@@\n\
+@@@@@@                                                                     @@@@@\n\
+@@@@@@@@                                                                 @@@@@@@\n\
+@@@@@@@@@@@                                                           @@@@@@@@@@\n\
+@@@@@@@@@@@@@@@                                                    @@@@@@@@@@@@@\n\
+@@@@@@@@@@@@@@@@@@@@                                         @@@@@@@@@@@@@@@@@@@\n\
+@@@@@@@@@@@@@@@@@@@@@@@@@@.                           .@@@@@@@@@@@@@@@@@@@@@@@@@\
+";
+
+    const char rocket2[] =
+            "           ^ \n\
+          /^\\\n\
+          |-|\n\
+          |M|\n\
+          |Y|\n\
+          | |\n\
+          |O|\n\
+          |S|\n\
+         /| |\\\n\
+        / | | \\\n\
+       |  | |  |\n\
+        `-\"\"\"-`\n\
+";
+
+
+
+
+
+        int jumpControlAtBottom = 0;
+        const int someDelay = 6000;
+        int shifControl = 0;
+
+
+        //jump to bottom of console
+
+        for (jumpControlAtBottom = 0; jumpControlAtBottom < 30; ++jumpControlAtBottom)
+        {
+            print_str("\n");
+        }
+
+        //Print rocket
+    print_str(rocket);
+
+        for (shifControl = 0; shifControl < 30; ++shifControl)
+        {
+            // Rocket move on the basis of delay
+            delay(someDelay);
+            delay(someDelay);
+            // move rocket a line upward
+            print_str("\n");
+        }
+
     print_menu();
 }
